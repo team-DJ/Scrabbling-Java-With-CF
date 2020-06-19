4b1d90fcaf0a527e5b58d598cf9279e8bd427503
team-DJ <67165186+team-DJ@users.noreply.github.com>
2020-06-20 02:52:04 +0530

    correct

--- Solution.java
+++ Solution.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public clas Solution {
+public class Solution {
     public static void main(String[] args) {
         // Create a Scanner object to read input from stdin.
         Scanner scan = new Scanner(System.in);
