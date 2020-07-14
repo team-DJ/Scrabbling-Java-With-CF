220ca5e4173a1050789623cb21edf9db425a0728
team-DJ <67165186+team-DJ@users.noreply.github.com>
2020-07-14 21:13:38 +0530

    I have made a cechk

---
+++ cechkscwj/areaofcirlce.java
@@ -0,0 +1,19 @@
+
+/**
+ * Program to print area of cirlce
+ */
+import java.util.Scanner;
+public class areaofcirlce
+{
+    public static void main (String args[])
+    
+{
+    Scanner vi=new Scanner(System.in);
+    double r, area;
+    System.out.println("Eneter radius");
+     r=vi.nextInt();
+    area=22.0/7.0*r*r;
+    System.out.println("Area is" + area);
+  
+}
+}
